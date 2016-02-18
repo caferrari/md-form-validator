3b8a230e6bf3f8684eb8ce334a3e78e3d4a7df89
Carlos André Ferrari <caferrari@gmail.com>
2016-02-18 18:05:27 -0200

    Fixed module

--- src/md-form-validator.js
+++ src/md-form-validator.js
@@ -1,7 +1,7 @@
 (angular => {
   'use strict';
 
-  angular.module('md.form.validator', [])
+  angular.module('md.form.validator')
   .directive('mdFormValidator', ['$compile', mdFormValidator]);
 
   function mdFormValidator($compile) {
